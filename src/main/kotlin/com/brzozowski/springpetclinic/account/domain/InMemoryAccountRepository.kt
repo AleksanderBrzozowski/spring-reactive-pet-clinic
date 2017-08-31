98029fa2ade815d5bb2d9938c31f2fa3c7fce2c8
Aleksander <olek.brzozowski@gmail.com>
2017-08-31 19:06:56 +0200

    refactor: add extension to Map class

--- src/main/kotlin/com/brzozowski/springpetclinic/account/domain/InMemoryAccountRepository.kt
+++ src/main/kotlin/com/brzozowski/springpetclinic/account/domain/InMemoryAccountRepository.kt
@@ -1,8 +1,8 @@
 package com.brzozowski.springpetclinic.account.domain
 
-import com.brzozowski.springpetclinic.infrastructure.extension.findByValue
+import com.brzozowski.springpetclinic.infrastructure.extension.reactiveFindByValue
+import com.brzozowski.springpetclinic.infrastructure.extension.reactivePutAndGetActual
 import reactor.core.publisher.Mono
-import reactor.core.publisher.toMono
 import java.util.concurrent.ConcurrentHashMap
 
 /**
@@ -13,30 +13,22 @@ class InMemoryAccountRepository : AccountRepository {
     val map = ConcurrentHashMap<String, Account>()
 
     override fun save(account: Account): Mono<Account> {
-        return Mono.fromCallable {
-            map.put(account.id, account)
-            val account1 = map[account.id]
-            account1
-        }
+        return map.reactivePutAndGetActual(account.id, account)
     }
 
     override fun findByEmail(email: String): Mono<Account> {
-        return find { it.email == email }
+        return map.reactiveFindByValue { it.email == email }
     }
 
     override fun findByUsername(username: String): Mono<Account> {
-        return find { it.username == username }
+        return map.reactiveFindByValue { it.username == username }
     }
 
     override fun findById(id: String): Mono<Account> {
-        return find { it.id == id }
+        return map.reactiveFindByValue { it.id == id }
     }
 
     private fun find(predicate: (Account) -> Boolean): Mono<Account> {
-        return Mono.defer {
-            map.findByValue(predicate)
-                    ?.toMono()
-                    ?: Mono.empty()
-        }
+        return map.reactiveFindByValue(predicate)
     }
 }
--- src/main/kotlin/com/brzozowski/springpetclinic/infrastructure/extension/MapExtension.kt
+++ src/main/kotlin/com/brzozowski/springpetclinic/infrastructure/extension/MapExtension.kt
@@ -1,18 +1,27 @@
 package com.brzozowski.springpetclinic.infrastructure.extension
 
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
 import java.lang.IllegalArgumentException
 
 /**
  * @author Aleksander Brzozowski
  */
 
-fun <K, V> Map<K, V>.findByValue(predicate: (V) -> Boolean): V? {
+fun <K, V> Map<K, V>.reactiveFindByValue(predicate: (V) -> Boolean): Mono<V> {
     val filteredMap = this.filterValues { predicate.invoke(it) }
     if (filteredMap.isEmpty()) {
-        return null
+        return Mono.empty()
     }else if (filteredMap.size > 1) {
         throw IllegalArgumentException("Map contains more than one element matching given predicate")
     } else {
-        return filteredMap.entries.iterator().next().value
+        return filteredMap.entries.iterator().next().value.toMono()
+    }
+}
+
+fun <K, V> MutableMap<K, V>.reactivePutAndGetActual(key: K, value: V): Mono<V> {
+    return Mono.fromCallable{
+        this.put(key, value)
+        this[key]
     }
 }
